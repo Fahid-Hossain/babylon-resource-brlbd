a41b9acbf67ad54deceac0b249612332e4cdffa2
Fahid-Hossain <fahidhossain2021@gmail.com>
2022-10-30 16:08:14 +0600

    header slider section done

--- src/components/Home/Slider/Slider.js
+++ src/components/Home/Slider/Slider.js
@@ -1,34 +1,73 @@
 import React from "react";
-import { Carousel } from "react-bootstrap";
 
 const Slider = () => {
   return (
     <>
-      <Carousel variant="dark">
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="https://img.freepik.com/free-vector/la-paz-bolivia-city-skyline-sky-background_596401-662.jpg?size=626&ext=jpg"
-            alt="First slide" style={{height:"100vh",opacity:"0.6"}}
-          />
-         
-          {/* <Carousel.Caption>
-            <h1>First slide label</h1>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption> */}
-        </Carousel.Item>
-        {/* <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="https://img.freepik.com/free-vector/flat-horizontal-banner-template-world-rabies-day_23-2149576978.jpg?w=1800&t=st=1666852968~exp=1666853568~hmac=8789f9cac094a502fadb0d1d23cc3c465ba2cae4c1bf998deb92d6a216e31897"
-            alt="Second slide"
-          /> */}
-          {/* <Carousel.Caption>
-            <h1>Second slide label</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption> */}
-        {/* </Carousel.Item> */}
-      </Carousel>
+     {/* <!-- Carousel wrapper --> */}
+<div id="carouselDarkVariant" class="carousel slide carousel-fade carousel-dark" data-mdb-ride="carousel">
+  {/* <!-- Indicators --> */}
+  <div class="carousel-indicators">
+    <button
+      data-mdb-target="#carouselDarkVariant"
+      data-mdb-slide-to="0"
+      class="active"
+      aria-current="true"
+      aria-label="Slide 1"
+    ></button>
+    <button
+      data-mdb-target="#carouselDarkVariant"
+      data-mdb-slide-to="1"
+      aria-label="Slide 1"
+    ></button>
+    <button
+      data-mdb-target="#carouselDarkVariant"
+      data-mdb-slide-to="2"
+      aria-label="Slide 1"
+    ></button>
+  </div>
+
+  {/* <!-- Inner --> */}
+  <div class="carousel-inner">
+    {/* <!-- Single item --> */}
+    <div class="carousel-item active">
+      <img src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(19).webp" class="d-block w-100" alt="Motorbike Smoke" style={{height:"100vh"}}/>
+      <div class="carousel-caption d-none d-md-block">
+        <h1>First slide label</h1>
+        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+      </div>
+    </div>
+
+    {/* <!-- Single item --> */}
+    <div class="carousel-item">
+      <img src="https://img.freepik.com/free-vector/la-paz-bolivia-city-skyline-sky-background_596401-662.jpg?size=626&ext=jpg" class="d-block w-100" alt="Mountaintop" style={{height:"100vh",opacity:"0.7"}}/>
+      <div class="carousel-caption d-none d-md-block">
+        <h1>Second slide label</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </div>
+    </div>
+
+    {/* <!-- Single item --> */}
+    <div class="carousel-item">
+      <img src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(40).webp" class="d-block w-100" alt="Woman Reading a Book" style={{height:"100vh"}}/>
+      <div class="carousel-caption d-none d-md-block">
+        <h1>Third slide label</h1>
+        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+      </div>
+    </div>
+  </div>
+  {/* <!-- Inner --> */}
+
+  {/* <!-- Controls --> */}
+  <button class="carousel-control-prev" type="button" data-mdb-target="#carouselDarkVariant" data-mdb-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-mdb-target="#carouselDarkVariant" data-mdb-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+{/* <!-- Carousel wrapper --> */}
     </>
   );
 };
