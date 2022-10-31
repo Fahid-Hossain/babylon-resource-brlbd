3859b7e83a189c9c27041671618f6ebac7b62892
Fahid-Hossain <fahidhossain2021@gmail.com>
2022-10-31 11:11:18 +0600

    form design modify

--- src/components/Forms/Forms.js
+++ src/components/Forms/Forms.js
@@ -1,7 +1,4 @@
 import React from "react";
-import { Controller, useForm } from "react-hook-form";
-// import "./Forms.css";
-import Select from "react-select";
 
 const Forms = () => {
   return (
@@ -11,11 +8,11 @@ const Forms = () => {
         <h2 className="text-secondary fw-bold mb-4"> Get in Touch </h2>
         <div class="row mb-4">
           <div class="col">
-            <div class="form-outline">
+            <div class="">
               <input 
                 type="text"
                 id="form3Example1"
-                class="form-control border border-primary rounded-pill"
+                class="form-control rounded-pill"
                 required
               />
               <label class="form-label" for="form3Example1">
@@ -24,11 +21,11 @@ const Forms = () => {
             </div>
           </div>
           <div class="col">
-            <div class="form-outline">
+            <div class="">
               <input
                 type="text"
                 id="form3Example2"
-                class="form-control border border-primary rounded-pill"
+                class="form-control rounded-pill"
                 required
               />
               <label class="form-label" for="form3Example2">
@@ -39,11 +36,11 @@ const Forms = () => {
         </div>
 
         {/* <!-- Email input --> */}
-        <div class="form-outline mb-4">
+        <div class="mb-4">
           <input
             type="email"
             id="form3Example3"
-            class="form-control border border-primary rounded-pill"
+            class="form-control rounded-pill"
             required
           />
           <label class="form-label" for="form3Example3">
@@ -52,8 +49,8 @@ const Forms = () => {
         </div>
 
         {/* <!-- Message input --> */}
-        <div class="form-outline mb-1">
-          <textarea class="form-control border border-primary rounded" id="form4Example3" rows="4"></textarea>
+        <div class="mb-1">
+          <textarea class="form-control rounded" id="form4Example3" rows="4"></textarea>
           <label class="form-label" for="form4Example3">
             Message
           </label>
@@ -73,52 +70,10 @@ const Forms = () => {
         </div>
 
         {/* <!-- Submit button --> */}
-        <button type="submit" class="btn btn-primary btn-rounded mb-4">
+        <div class="d-flex justify-content-end">
+        <button type="submit" class="btn btn-primary btn-rounded mb-4 ms-2">
           Send <i class="fa fa-solid fa-paper-plane"></i>
         </button>
-
-        {/* <!-- Register buttons --> */}
-
-        <div class="text-center">
-          <p>or connect with:</p>
-          {/* <!-- Google --> */}
-          <a
-            class="btn text-white mx-1"
-            style={{ backgroundColor: "#dd4b39" }}
-            href="#!"
-            role="button"
-          >
-            <i class="fab fa-google"></i>
-          </a>
-          {/* <!-- Facebook --> */}
-          <a
-            class="btn text-white mx-1"
-            style={{ backgroundColor: "#3b5998" }}
-            href="#!"
-            role="button"
-          >
-            <i class="fa fa-brands fa-facebook"></i>
-          </a>
-
-          {/* <!-- Twitter --> */}
-          <a
-            class="btn text-white mx-1"
-            style={{ backgroundColor: "#55acee" }}
-            href="#!"
-            role="button"
-          >
-            <i class="fab fa-twitter"></i>
-          </a>
-
-          {/* <!-- Twitter --> */}
-          <a
-            class="btn text-white mx-1"
-            style={{ backgroundColor: "#ac2bac" }}
-            href="#!"
-            role="button"
-          >
-            <i class="fab fa-github"></i>
-          </a>
         </div>
       </form>
     </div>
--- src/components/Home/Slider/Slider.js
+++ src/components/Home/Slider/Slider.js
@@ -30,29 +30,29 @@ const Slider = () => {
   <div class="carousel-inner">
     {/* <!-- Single item --> */}
     <div class="carousel-item active">
-      <img src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(19).webp" class="d-block w-100" alt="Motorbike Smoke" style={{height:"100vh"}}/>
-      <div class="carousel-caption d-none d-md-block">
+      <img src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(19).webp" class="d-block w-100" alt="Motorbike Smoke" style={{height:"80vh"}}/>
+      {/* <div class="carousel-caption d-none d-md-block">
         <h1>First slide label</h1>
         <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-      </div>
+      </div> */}
     </div>
 
     {/* <!-- Single item --> */}
     <div class="carousel-item">
-      <img src="https://img.freepik.com/free-vector/la-paz-bolivia-city-skyline-sky-background_596401-662.jpg?size=626&ext=jpg" class="d-block w-100" alt="Mountaintop" style={{height:"100vh",opacity:"0.7"}}/>
-      <div class="carousel-caption d-none d-md-block">
+      <img src="https://img.freepik.com/free-vector/la-paz-bolivia-city-skyline-sky-background_596401-662.jpg?size=626&ext=jpg" class="d-block w-100" alt="Mountaintop" style={{height:"80vh",opacity:"0.7"}}/>
+      {/* <div class="carousel-caption d-none d-md-block">
         <h1>Second slide label</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-      </div>
+      </div> */}
     </div>
 
     {/* <!-- Single item --> */}
     <div class="carousel-item">
-      <img src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(40).webp" class="d-block w-100" alt="Woman Reading a Book" style={{height:"100vh"}}/>
-      <div class="carousel-caption d-none d-md-block">
+      <img src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(40).webp" class="d-block w-100" alt="Woman Reading a Book" style={{height:"80vh"}}/>
+      {/* <div class="carousel-caption d-none d-md-block">
         <h1>Third slide label</h1>
         <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-      </div>
+      </div> */} 
     </div>
   </div>
   {/* <!-- Inner --> */}
